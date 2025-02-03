79c90dd852b0144345afbc7f3cbc038b9de38a71
SeniorCod-72 <wwekesa72@gmail.com>
2025-02-03 18:18:37 +0300

    done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,29 @@
 import { NavLink } from "react-router-dom";
+import "./NavBar.css"
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
+    <nav>
+      <NavLink
+        to="/"
+        className="nav-link"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className="nav-link"
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        className="nav-link"
+      >
+        Login
+      </NavLink>
     </nav>
-    );
-}
+  );
+};
 
 export default NavBar;
